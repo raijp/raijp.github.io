766722d0b49056f19090fd7af94c1f24bddd7c06
raijp <rai.jp.work@gmail.com>
2022-12-05 05:07:54 +0900

    m

--- wasm/tmp/test.c
+++ wasm/tmp/test.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BUFSIZE 16
+#define BUFSIZE 24
 int main() {
   char buf1[BUFSIZE];
-  char buf2[BUFSIZE] = "buf2buf2buf2buf2";
-  char * buf3 = "123456789abcdefghijklmnopqrstuvwxyz";
-  strncpy(buf1, buf3, 16);
+  char buf2[BUFSIZE] = "\"><script>alert</script>";
+  char * buf3 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+  strncpy(buf1, buf3, 24);
   printf("%s\n", buf1);
   printf("%s\n", buf2);
   printf("%s\n", buf3);
