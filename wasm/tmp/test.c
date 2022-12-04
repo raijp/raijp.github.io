6b4f97864b37a11d2eb76c841d8809869c0355f0
raijp <rai.jp.work@gmail.com>
2022-12-05 05:11:20 +0900

    m

--- wasm/tmp/test.c
+++ wasm/tmp/test.c
@@ -6,7 +6,7 @@ int main() {
   char buf1[BUFSIZE];
   char buf2[BUFSIZE] = "\"><script>alert()</script>";
   char * buf3 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-  strncpy(buf1, buf3, 24);
+  strncpy(buf1, buf3, BUFSIZE);
   printf("%s\n", buf1);
   printf("%s\n", buf2);
   printf("%s\n", buf3);
